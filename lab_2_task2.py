bb2df90cde8e8a19c34bac3ca98243136565da74
moaxgetsu <65217332+Nmxxxxx@users.noreply.github.com>
2024-11-02 19:02:48 +0200

    Update lab_2_task2.py

--- lab_2_task2.py
+++ lab_2_task2.py
@@ -1,1 +1,22 @@
 
+def operation_decorator(func):
+    def wrapper(x, y, operation):
+        if operation == '+':
+            return func(x, y, lambda x, y: x + y)
+        elif operation == '-':
+            return func(x, y, lambda x, y: x - y)
+        elif operation == '*':
+            return func(x, y, lambda x, y: x * y)
+        elif operation == '/':
+            if y != 0:
+                return func(x, y, lambda x, y: x / y)
+            else:
+                return "Error: Division by zero"
+    return wrapper
+
+@operation_decorator
+def calculate(x, y, operation_func):
+    return operation_func(x, y)
+
+result = calculate(5, 3, '+')
+print(result)
